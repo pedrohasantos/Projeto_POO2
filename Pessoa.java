a06894f9cb91235471134c01865b6922a162d0e1
pedrohasantos <133005154+pedrohasantos@users.noreply.github.com>
2024-04-27 16:49:54 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,1 +1,47 @@
+class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
 
+    public Pessoa(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void printInfo() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Telefone: " + telefone);
+    }
+
+    public void imprimirDetalhes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Telefone: " + telefone);
+    }
+}
