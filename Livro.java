2be51e72431ac059f655d5e10a01e9cfa8481c9b
pedrohasantos <133005154+pedrohasantos@users.noreply.github.com>
2024-04-27 16:50:21 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -1,1 +1,55 @@
+class Livro {
+    private String titulo;
+    private String autor;
+    private int idLivro;
+    private String status;
 
+    public Livro(String titulo, String autor, int idLivro, String status) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.idLivro = idLivro;
+        this.status = status;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getIdLivro() {
+        return idLivro;
+    }
+
+    public void setIdLivro(int idLivro) {
+        this.idLivro = idLivro;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", idLivro=" + idLivro +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
