044ad70d4a62a76df6d83140f5c5f29c77d49da9
Renan-ribeiro10 <133006114+Renan-ribeiro10@users.noreply.github.com>
2024-04-29 17:30:40 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
+
+//Define a classe Cliente, que estende a classe Pessoa, o que sugere que Cliente herda características de uma pessoa.
 class Cliente extends Pessoa {
     private int idCliente;
     private ArrayList<Livro> livrosEmprestados;
 
+    //O construtor inicializa um cliente com seu nome, endereço, telefone e um identificador único. Além disso, inicializa a lista de livros emprestados como uma nova instância de ArrayList.
     public Cliente(String nome, String endereco, String telefone, int idCliente) {
         super(nome, endereco, telefone);
         this.idCliente = idCliente;
         this.livrosEmprestados = new ArrayList<>();
     }
 
+    //Métodos para acessar e modificar o identificador do cliente e a lista de livros emprestados.
     public int getIdCliente() {
         return idCliente;
     }
@@ -24,17 +28,19 @@ class Cliente extends Pessoa {
     public void setLivrosEmprestados(ArrayList<Livro> livrosEmprestados) {
         this.livrosEmprestados = livrosEmprestados;
     }
-
+    //Adiciona um livro à lista de livros emprestados pelo cliente e atualiza o status do livro para "Emprestado".
     public void emprestarLivro(Livro livro) {
         livrosEmprestados.add(livro);
         livro.setStatus("Emprestado");
     }
-
+    
+    //Remove um livro da lista de livros emprestados pelo cliente e atualiza o status do livro para "Disponível".
     public void devolverLivro(Livro livro) {
         livrosEmprestados.remove(livro);
         livro.setStatus("Disponível");
     }
-
+    
+    //Este método verifica se há livros emprestados que estão atrasados para devolução e imprime uma mensagem para cada livro atrasado.
     public void verificarLivrosAtrasados() {
         boolean atrasado = false;
         for (Livro livro : livrosEmprestados) {
@@ -47,7 +53,8 @@ class Cliente extends Pessoa {
             System.out.println("Em caso de atraso na devolução, multa ao dia no valor de 75 centavos.");
         }
     }
-
+    
+    //Sobrescreve o método toString() para retornar uma representação em forma de string do objeto Cliente, incluindo o identificador do cliente e a lista de livros emprestados.
     @Override
     public String toString() {
         return "Cliente{" +
