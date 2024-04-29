e4dfdc68c6fe823c21551f6381317a7dd2e284bd
Renan-ribeiro10 <133006114+Renan-ribeiro10@users.noreply.github.com>
2024-04-29 17:34:01 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -3,14 +3,16 @@ class Livro {
     private String autor;
     private int idLivro;
     private String status;
-
+    
+    //O construtor inicializa um objeto Livro com o título, autor, identificador único e status fornecidos.
     public Livro(String titulo, String autor, int idLivro, String status) {
         this.titulo = titulo;
         this.autor = autor;
         this.idLivro = idLivro;
         this.status = status;
     }
-
+    
+    //Esses métodos permitem acessar e modificar os atributos do livro (título, autor, identificador e status).
     public String getTitulo() {
         return titulo;
     }
@@ -42,7 +44,8 @@ class Livro {
     public void setStatus(String status) {
         this.status = status;
     }
-
+    
+    //Sobrescreve o método toString() para retornar uma representação em forma de string do objeto Livro, incluindo título, autor, identificador e status.
     @Override
     public String toString() {
         return "Livro{" +
